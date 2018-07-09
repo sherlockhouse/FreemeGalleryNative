8754720f4b15fad29e1e14747294a882fc07ac4d
gulincheng <gulincheng@droi.com>
2018-07-09 11:22:46 +0800

    FreemeGalleryO bugfix#0037567: adjust slotview in select mode
    Change-Id: Ie7bf6ad137988f1ee8f24a6205e8e6884e849452
    Signed-off-by: gulincheng <gulincheng@droi.com>

--- app/src/main/src/com/android/gallery3d/app/Config.java
+++ app/src/main/src/com/android/gallery3d/app/Config.java
@@ -59,7 +59,7 @@ final class Config {
             slotViewSpec.slotHeightAdditional = 0;
             //*/Added by droi Linguanrong for Gallery new style, 2013-12-17
             slotViewSpec.slotPadding = r.getDimensionPixelSize(R.dimen.album_slot_padding);
-            slotViewSpec.bottomPadding = r.getDimensionPixelSize(R.dimen.tab_bar_default_height);
+            slotViewSpec.bottomPadding = r.getDimensionPixelSize(R.dimen.albumset_bottompadding);
             paddingLeftRight = r.getDimensionPixelSize(R.dimen.albumstory_padding_left_right);
 
             slotViewSpec.slotGapV = r.getDimensionPixelSize(R.dimen.albumstory_slot_gap_vertical);
